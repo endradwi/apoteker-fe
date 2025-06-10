6eb701ee53d07d33838d5c502655fdae04c89c15
Endra Dwi <endradwijamludin@gmail.com>
2025-06-10 17:36:55 +0700

    chore update middlware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,9 +1,36 @@
+import Swal from "sweetalert2"
+
 export default defineNuxtRouteMiddleware((to) => {
   const token = useCookie('token')
   const isProtected =
-    to.path === '/regis' || to.path.startsWith('/admin')
+    to.path === '/regis' || to.path === '/history' || to.path.startsWith('/admin')
 
   if (isProtected && !token.value) {
-    return navigateTo('/')
+    if (process.client) {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Akses Ditolak',
+        text: 'Anda harus login terlebih dahulu untuk mengakses halaman ini.',
+        timer: 3000,
+        timerProgressBar: true,
+        showConfirmButton: false,
+      }).then(() => {
+        window.location.href = '/' // Gunakan ini karena `router.push()` tidak bisa di middleware secara langsung
+      })
+    }
+
+    return abortNavigation() // Ini menghentikan navigasi tanpa error 500
   }
 })
+
+// if (isProtected && !token.value) {
+//   return navigateTo('/unauthorized')
+// }
+
+// halaman custom //Anauthorized
+// <template>
+//   <div class="text-center mt-10">
+//     <h1 class="text-2xl font-bold text-red-600">Akses Ditolak</h1>
+//     <p>Silakan login terlebih dahulu untuk mengakses halaman ini.</p>
+//   </div>
+// </template>

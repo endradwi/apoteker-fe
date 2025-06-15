a49bdd3540a9836d8fe1064bd7a595fd4f9d499e
Endra Dwi <endradwijamludin@gmail.com>
2025-06-15 18:05:46 +0700

    chore: cek

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,44 +1,37 @@
 import Swal from "sweetalert2"
 
 export default defineNuxtRouteMiddleware(async (to) => {
-  if (process.server) return  // ✅ Hindari SSR
+  if (process.server) return  // Hindari SSR
 
-  const token = useCookie('token')
-
+  const token = useCookie('token', { sameSite: 'none' })  // Optional
   console.log("Token from client cookie:", token.value)
+
   const isProtected =
-    to.path === '/regis' || to.path === '/history' || to.path === '/profile' || to.path.startsWith('/admin')
+    ['/regis', '/history', '/profile'].includes(to.path) || to.path.startsWith('/admin')
 
-  console.log("token from middleware:", token.value)
-
-  // ⛔ Tidak ada token tapi akses halaman protected
-  if (!token.value && isProtected ) {
+  if (!token.value && isProtected) {
     await Swal.fire({
       icon: 'warning',
       title: 'Akses Ditolak',
-      text: 'Anda harus login terlebih dahulu untuk mengakses halaman ini.',
+      text: 'Anda harus login terlebih dahulu.',
       timer: 3000,
-      timerProgressBar: true,
       showConfirmButton: false,
     })
     return navigateTo('/')
   }
 
-  // ✅ Jika ada token, cek role
   if (token.value && to.path.startsWith('/admin')) {
     const store = useUserStore()
     const data = await store.profile() as { results?: { role_id: number } }
-    if (data?.results?.role_id !== 1) {
+    if (data.results?.role_id !== 1) {
       await Swal.fire({
         icon: 'warning',
         title: 'Akses Ditolak',
-        text: 'Halaman ini hanya bisa diakses oleh admin.',
+        text: 'Halaman hanya untuk admin.',
         timer: 3000,
-        timerProgressBar: true,
         showConfirmButton: false,
       })
       return navigateTo('/')
     }
   }
-  
 })

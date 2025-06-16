60e91a2c4bdcbdf92a4f98446e394eba400fe70e
Endra Dwi <endradwijamludin@gmail.com>
2025-06-16 14:16:13 +0700

    chore: cek

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -3,17 +3,14 @@ import Swal from "sweetalert2"
 export default defineNuxtRouteMiddleware(async (to) => {
   if (process.server) return  // Hindari SSR
 
-  const token = useCookie('token', {
-    sameSite: 'none',
-    secure: true,
-  })
-  console.log("Cek Token:", document.cookie.includes('token'))
-  console.log("Token from client cookie:", token.value)
+  const tokenFromCookie = useCookie('token')
+  const tokenFromLocalStorage = useCredentialsStore().loadToken()
+  console.log('Token from cookie:', tokenFromCookie.value)
 
   const isProtected =
     ['/regis', '/history', '/profile'].includes(to.path) || to.path.startsWith('/admin')
 
-  if (!token.value && isProtected) {
+  if (!tokenFromCookie.value && isProtected) {
     await Swal.fire({
       icon: 'warning',
       title: 'Akses Ditolak',
@@ -24,7 +21,7 @@ export default defineNuxtRouteMiddleware(async (to) => {
     return navigateTo('/')
   }
 
-  if (token.value && to.path.startsWith('/admin')) {
+  if (tokenFromCookie.value && to.path.startsWith('/admin')) {
     const store = useUserStore()
     const data = await store.profile() as { results?: { role_id: number } }
     if (data.results?.role_id !== 1) {
